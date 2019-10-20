460d74008e242bf201f75ae33bf2d736daf55375
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-20 08:59:37 +0100

    profile page updated

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { MultiSelectComponent } from '@syncfusion/ej2-react-dropdowns';
-import { Form as AntForm, Icon, Input, Button, Checkbox } from 'antd';
+import { withStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { FaCommentAlt, FaUserEdit, FaBriefcase } from 'react-icons/fa';
 
-// Summary, skills, rate, Role, Image, Name, Location
-
+// Summary, ratings, skills, rate, Role, Image, Name, Location
+let person={};
 export default function Profile(props) {
   const onLogin = ({ username, password }) => {
     return props.onLogin({ username, password });
   };
-  console.log(props.userDetails);
+  if(props.userDetails.person)console.log(props.userDetails.person);
+  if(props.userDetails.person){
+    person=props.userDetails.person;
+  }
+
+  const useStyles = makeStyles(theme => ({
+    progress: {
+      margin: theme.spacing(2),
+    },
+  }));
+
+  const classes = useStyles();
+
+  const StyledRating = withStyles({
+    iconFilled: {
+      display: 'flex',
+      color: '#FF3366',
+      fontSize: '25pt',
+      alignSelf: 'center',
+    },
+    iconHover: {
+      color: '#ff3d47',
+    },
+  })(Rating);
+
   return (
     <div className='profile-form'>
+    {!person._profile &&  (<CircularProgress className={classes.progress} />)}
+    {person._profile && (
+    <>
+    <div className='profile-info'>
+      <img src={person._profile.image["0"].contentUrl} alt='profile'/>
+        <div className='profile-others'>
+          <h2>{person._profile.name}</h2>
+          <h3>{props.userDetails.username}</h3>
+          <div className='ratings' ><p>Ratings:   <br/><StyledRating
+            name="customized-color"
+            value={5}
+            // getLabelText={getLabelText}
+            precision={0.5}
+            icon={<FavoriteIcon fontSize="inherit" />}
+            readOnly
+          /></p></div>
+          <h3>Skills: </h3>
+        </div>
+        <div className='buttons'>
+          <button className='edit-button'>
+            <FaUserEdit/>
+            EDIT
+          </button>
+          <button className='hire-button'>
+          <FaBriefcase />
+          HIRE
+          </button>
+          <button className='msg-button'>
+          <FaCommentAlt />
+          MESSAGE
+          </button>
+        </div>
+    </div>
     <Formik
       
       initialValues={{ username: '', password: '' }}
@@ -23,6 +86,8 @@ export default function Profile(props) {
           <input type='submit' />
         </Form>
       )}
-    /></div>
+    />
+    </>)}
+    </div>
   );
 }
